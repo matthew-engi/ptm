4b5da654aefda51b2c61a011cff795b9657de599
matthew-engi <148574010+matthew-engi@users.noreply.github.com>
2026-08-06 12:28:28 -0400

    style(struct): Better grouping in ptm.zig for clarity

--- ptm.zig
+++ ptm.zig
@@ -1,13 +1,23 @@
 // Allow for internal access to libraries
-pub const Color = @import("./src/color.zig").Color;
-pub const Image = @import("./src/image.zig").Image;
-pub const Matrix = @import("./src/matrix.zig").Matrix;
+
+/// Struct containing inner-structs not typically needed outside.
+pub const Utilities = struct {
+   pub const Color = @import("./src/color.zig").Color;
+   pub const Image = @import("./src/image.zig").Image;
+   pub const Matrix = @import("./src/matrix.zig").Matrix;
+};
 
 // Access to PTM file structs
 const ptm = @import("./src/mod.zig");
 
-pub const PTM = ptm.PTM;
-pub const Header = ptm.Header;
-pub const Sprite = ptm.Sprite;
+/// PTM file format types
+pub const File = struct {
+   pub const PTM = ptm.PTM;
+   pub const Header = ptm.Header;
+   pub const Sprite = ptm.Sprite;
+};
+
+/// Reader struct to read PTM files
 pub const Reader = @import("./src/reader.zig").Reader;
+/// Writer struct to write PTM files
 pub const Writer = @import("./src/writer.zig").Writer;
