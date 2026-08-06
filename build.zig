ce979c25e7b44e84f1d2280bd5750febc1d301e7
matthew-engi <148574010+matthew-engi@users.noreply.github.com>
2026-08-06 12:13:39 -0400

    Other import changes

--- build.zig
+++ build.zig
@@ -4,9 +4,11 @@ pub fn build(b: *Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("ptm", .{
+    const ptm_module = b.addModule("ptm", .{
         .root_source_file = b.path("ptm.zig"),
         .target = target,
         .optimize = optimize,
     });
+
+    ptm_module.addImport("ptm", ptm_module);
 }
